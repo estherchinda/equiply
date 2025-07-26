c4c2a7348b7167dc53f3a7c03a25cb5ad5cd814b
estherchinda <davidakachi9@gmail.com>
2025-07-26 14:20:59 +0100

    testing deployment

--- app/user/tools/[toolsId]/request/page.tsx
+++ app/user/tools/[toolsId]/request/page.tsx
@@ -1,33 +1,3 @@
-// "use client";
-
-// import Heading from "@/ui/display/HeadingComponent";
-// import Button from "@/ui/forms/Button";
-// import DateRangePicker from "@/ui/forms/DateRangePicker";
-
-// export default function RequestPage() {
-//     return (
-//         <section>
-//             <p className="text-sm font-medium leading-6 space-x-2">
-//                 <span className="text-[#94C7A8]">Equipment</span>
-//                 <span className="text-[#94C7A8]">/</span>
-//                 <span>Choose Dates</span>
-//             </p>
-//             <div className="mt-4 space-y-3">
-//                 <Heading content="Choose Dates" />
-//                 <p className="text-sm leading-5 text-[#9EB8A8]">Select the dates you&apos;d like to rent the equipment for.</p>
-//                 <DateRangePicker />
-//                 <Heading content="Availability" />
-//                 <p className="text-sm leading-5 text-white">Please select a date range to see the availability.</p>
-//                 <Heading content="Price" />
-//                 <p className="text-sm leading-5 text-white">Please select a date range to see the price.</p>
-//                 <div className="mt-4 w-[80%]">
-//                     <Button content="Continue" />
-//                 </div>
-//             </div>
-//         </section>
-//     )
-// }
-
 "use client";
 
 import Heading from "@/ui/display/HeadingComponent";
@@ -40,7 +10,7 @@ type DateRange = {
   to?: Date;
 };
 
-type Props = {
+type CProps = {
   onChange?: (range: DateRange) => void;
   onContinue?: (range: DateRange) => void;
 };
@@ -193,7 +163,7 @@ const CalendarMonth = ({
   );
 };
 
-export default function DualCalendarPicker({ onChange, onContinue }: Props) {
+export default function DualCalendarPicker({ onChange, onContinue }: CProps) {
 const {
   selectedRange,
   currentMonth,
