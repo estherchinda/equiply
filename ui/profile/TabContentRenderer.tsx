ec817bbc8f3a980356c3653dc8feaee0d6fb9b87
estherchinda <davidakachi9@gmail.com>
2025-07-26 14:36:51 +0100

    facing one too many error

--- ui/profile/TabContentRenderer.tsx
+++ ui/profile/TabContentRenderer.tsx
@@ -1,21 +1,21 @@
 "use client";
 
-import TabComponent from "@/ui/display/TabComponent"
-import { tabsList } from "@/types/tabs"
-import { usePathname } from "next/navigation"
+// import TabComponent from "@/ui/display/TabComponent"
+// import { tabsList } from "@/types/tabs"
+// import { usePathname } from "next/navigation"
 
 export default function TabContentRenderer () {
-    const pathname = usePathname();
+    // const pathname = usePathname();
     return (
         <div className="border-b border-[#366347] flex items-center justify-center md:justify-start gap-8 py-1 md:px-3 my-4">
-            {tabsList.map((tab) => (
+            {/* {tabsList.map((tab) => (
               <TabComponent
                 key={tab.id}
                 tabName={tab.tabName}
                 href={tab.href}
                 pathname={pathname}
               />
-            ))}
+            ))} */}
         </div>
     )
 }
