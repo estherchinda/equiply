7eef458632566c25fce8265bf161e6e708e0b831
estherchinda <davidakachi9@gmail.com>
2025-07-26 14:32:49 +0100

    facing one too many errors

--- ui/messages/MessagesSideBar.tsx
+++ ui/messages/MessagesSideBar.tsx
@@ -1,29 +1,29 @@
 "use client";
-import TabComponent from "@/ui/display/TabComponent"
-import { usePathname } from "next/navigation";
-import { useState } from "react";
+// import TabComponent from "@/ui/display/TabComponent"
+// import { usePathname } from "next/navigation";
+// import { useState } from "react";
 
 export default function MessageSidebar() {
-    const pathname = usePathname();
-    const tabsList = [
-        {
-            id: 1,
-            tabName: "Chats",
-            href: "/user/messages/chats",
-        },
-        {
-            id: 2,
-            tabName: "Requests",
-            href: "/user/messages/requests",
-        },
-    ];
+    // const pathname = usePathname();
+    // const tabsList = [
+    //     {
+    //         id: 1,
+    //         tabName: "Chats",
+    //         href: "/user/messages/chats",
+    //     },
+    //     {
+    //         id: 2,
+    //         tabName: "Requests",
+    //         href: "/user/messages/requests",
+    //     },
+    // ];
 
-    const [activeTab, setActiveTab] = useState(1);
+    // const [activeTab, setActiveTab] = useState(1);
 
     return (
         <aside className="w-[320px] border-r border-[#E5E8EB]">
             <div className="border-b border-[#366347] flex items-center justify-center md:justify-start gap-8 py-1 md:px-3 my-4 mr-6">
-                {tabsList.map((tab) => (
+                {/* {tabsList.map((tab) => (
                 <TabComponent
                     key={tab.id}
                     index={tab.id}
@@ -32,7 +32,7 @@ export default function MessageSidebar() {
                     pathname={pathname}
                     activeTab={activeTab}
                 />
-                ))}
+                ))} */}
             </div>
         </aside>
     );
